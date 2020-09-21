2b886125de78d9817493383f3e4a1c6ead0f736a
SergSkm <serg.skm@yandex.ru>
2020-09-21 15:05:06 +0300

    Added case 2 for adding back zero-shock vs shock difference

--- app.py
+++ app.py
@@ -38,9 +38,10 @@ def get_ALM_forecasts():
         model1 = post_request_object['model1']
     if post_request_object['model2'] in models_all:
         model2 = post_request_object['model2']
+        
+    
 
-
-    rates_delta_forecast, rates_delta_forecast_base, mrktShare_delta_forecast = [], [], []
+    rates_delta_forecast, rates_delta_forecast_base, mrktShare_delta_forecast, mrktShare_delta_forecast_base = [], [], [], []
 
     
 #     # Case 1. General forecasts
@@ -63,6 +64,16 @@ def get_ALM_forecasts():
 
 #         mrktShare_delta_forecast.append(np.float(modelFitted2.predict(mrktShare_input.reshape(-1,2))))
         
+#     rates_forecast = np.array(post_request_object['rates_fact']) + np.array(rates_delta_forecast)
+#     mrktShare_forecast = np.array(post_request_object['mrktShare_fact']) + np.array(mrktShare_delta_forecast)
+
+#     py_response['rates_delta_forecast'] = list(rates_delta_forecast)
+#     py_response['mrktShare_delta_forecast'] = list(mrktShare_delta_forecast)
+
+#     py_response['rates_forecast'] = list(rates_forecast)
+#     py_response['mrktShare_forecast'] = adjust_MrktShare_To100(mrktShare_forecast)
+    
+    
     # Case 2. Difference between shocked case and zer0-shock case is added to the fact!
     for tenor, rates_input in zip(['1w', '2w', '3w', '1m', '2m', '3m', '6m'], 
                                   np.stack((post_request_object['ftp_delta_shocked'],
@@ -83,22 +94,36 @@ def get_ALM_forecasts():
                                      post_request_object['sector'] + "_" + tenor + ".pkl", "rb" )) 
 
         rates_delta_forecast_base.append(np.float(modelFitted1.predict(rates_input.reshape(-1,3)))) 
-        
+
+
     for tenor, mrktShare_input in zip(['1w', '2w', '3w', '1m', '2m', '3m', '6m'], 
                                   np.stack((post_request_object['mosprime_delta'], 
-                                            np.array(rates_delta_forecast) - np.array(rates_delta_forecast_base)), 
+                                            rates_delta_forecast), 
                                            axis=1)):
 
         base_directory = "models/model2/" + model2 + "/" + post_request_object['sector'] + "_"
         modelFitted2 = pickle.load(open(base_directory + tenor + ".pkl", "rb" )) 
 
         mrktShare_delta_forecast.append(np.float(modelFitted2.predict(mrktShare_input.reshape(-1,2))))  
-        
-        
-        
-    # I MULTIPLY BY 5 TO MAGNIFY CHANGE!!
-    rates_forecast = np.array(post_request_object['rates_fact']) + np.array(rates_delta_forecast)
-    mrktShare_forecast = np.array(post_request_object['mrktShare_fact']) + np.array(mrktShare_delta_forecast)*5
+
+
+    for tenor, mrktShare_input in zip(['1w', '2w', '3w', '1m', '2m', '3m', '6m'], 
+                                  np.stack((post_request_object['mosprime_delta'], 
+                                            rates_delta_forecast_base), 
+                                           axis=1)):
+
+        base_directory = "models/model2/" + model2 + "/" + post_request_object['sector'] + "_"
+        modelFitted2 = pickle.load(open(base_directory + tenor + ".pkl", "rb" )) 
+
+        mrktShare_delta_forecast_base.append(np.float(modelFitted2.predict(mrktShare_input.reshape(-1,2))))   
+
+    # adding back differences between shocked case and zero-shock case:
+    rates_forecast_difference = np.array(rates_delta_forecast) - np.array(rates_delta_forecast_base)
+    mrktShare_forecast_difference = np.array(mrktShare_delta_forecast) - np.array(mrktShare_delta_forecast_base)
+
+    rates_forecast = np.array(post_request_object['rates_fact']) + rates_forecast_difference
+    mrktShare_forecast = np.array(post_request_object['mrktShare_fact']) + mrktShare_forecast_difference
+
 
     py_response['rates_delta_forecast'] = list(rates_delta_forecast)
     py_response['mrktShare_delta_forecast'] = list(mrktShare_delta_forecast)
